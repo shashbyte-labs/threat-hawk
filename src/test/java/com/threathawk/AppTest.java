6f7956b4be14d61b1e026fc89f6d61a56d95c3be
shashbyte-labs <stiwari@kognivera.com>
2025-08-28 01:50:39 +0530

    Initial Setup: Main application class, pom.xml and small introduction of the app in readme

---
+++ src/test/java/com/threathawk/AppTest.java
@@ -0,0 +1,13 @@
+package com.threathawk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+        // This test just checks if the Spring application starts correctly
+    }
+}
